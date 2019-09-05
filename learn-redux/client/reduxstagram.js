2b7a5daadbfab01a1161f9ff0a9d4cc55112185a
mike last <mike.last@rokkmedia.co.uk>
2019-09-05 09:00:09 +0100

    added an app component which connects our store and passes stuff down

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -6,7 +6,7 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 // components
-import Main from './components/Main';
+import App from './components/App';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
@@ -17,10 +17,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history }  from './store';
 
+
 const router = (
     <Provider store={store}>
         <Router history={history}>
-            <Route path="/" component={Main}>
+            <Route path="/" component={App}>
                 <IndexRoute component={PhotoGrid} />
                 <Route path="/view/:postId" component={Single} />
             </Route>
